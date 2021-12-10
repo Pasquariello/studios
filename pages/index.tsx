864c8203a01aa774b0284a75aba04b87b29e35d1
Taylor Pasquariello <taylorpasquariello@Taylors-MacBook-Pro-2.local>
2021-12-10 01:47:05 -0700

    fix connection spelling mistake

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,7 +74,7 @@ const Home: NextPage = () => {
 
             variants={cardVariants}
           >
-          <h3>Connetion</h3> 
+          <h3>Connection</h3> 
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
           </motion.div>
 
