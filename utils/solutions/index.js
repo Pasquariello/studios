17035c6297c2c9dfd497063066b2c3a70fbdeb96
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-09-13 12:44:35 -0700

    fix coming soon body

--- utils/solutions/index.js
+++ utils/solutions/index.js
@@ -4,7 +4,7 @@ export const solutions = [
     {
         id: 0,
         title: `Solutions Coming Soon`,
-        body: MotiveCaseStudy,
+        body: null,
         image: '/static/images/andrew-neel-coffee.jpg',
         preview: 'We\'ve been hard at work delivering high quality software for our clients! We are currently writting new case studies to share with you as you\'re reading this.  If you\'d like to learn more send us an email so we can connect.  Thank you!'
     },
