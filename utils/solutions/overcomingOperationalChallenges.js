cbd4e5bdda64b1f49627dc0d926a652e7d308590
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-10-11 16:44:12 -0700

    fix mobiel titles

--- utils/solutions/overcomingOperationalChallenges.js
+++ utils/solutions/overcomingOperationalChallenges.js
@@ -1,19 +1,19 @@
 export const OvercomingOperationalChallenges = `
 	<h1>Overcoming Operational Challenges: How Automation Enhanced Clinician Efficiency and Improved Patient Care for a Virtual Health Platform</h1>
 	<p>10/10/24</p>
-	<h2>Executive Summary</h2>
+	<h3>Executive Summary</h3>
 
 	<p>
 		A leading virtual cardiac wellness platform aimed to improve both clinician experience and operational efficiency. After facing significant challenges in clinician and workflow management, the company partnered with Cascadian Software to introduce automation, streamline data access, and enhance technology infrastructure. These changes resulted in faster onboarding, reduced administrative burdens, and more informed decision-making, enabling clinicians to focus more on patient care.
 	</p>
-	<h2>
+	<h3>
 		Introduction
-	</h2>
+	</h3>
 	<p>
 		This innovative virtual cardiac wellness platform provides personalized digital care programs in collaboration with clinicians. Focused on revolutionizing cardiac health management through a behavior change approach, the company has always prioritized high-quality care. However, operational obstacles disrupted the clinical team&rsquo;s workflow, limiting their ability to fully focus on patient care.
 	</p>
 
-	<h2>The Challenge</h2>
+	<h3>The Challenge</h3>
 
 	<p>
 		The company encountered several key challenges that hindered efficiency:
@@ -24,7 +24,7 @@ export const OvercomingOperationalChallenges = `
 		<li><b>Data Accessibility Issues:</b> Limited access to patient data made it difficult for the company to make timely, data-driven decisions.</li>
 	</ul>
 	<p>These issues highlighted the need for a streamlined solution to improve both operational performance and clinician experience.</p>
-	<h2>The Solution</h2>
+	<h3>The Solution</h3>
 	<p>To address these challenges, the platform partnered with Cascadian Software to implement an advanced suite of automation and system integrations. Key actions included:</p>
 	<ul>
 		<li><b>Automating Administrative Tasks:</b> Using AWS Lambda and EC2 functions, the platform automated routine tasks such as patient care plan management, syncing Fitbit data, and tracking patient outreach.</li>
@@ -32,13 +32,13 @@ export const OvercomingOperationalChallenges = `
 		<li><b>Enhanced Patient Portal:</b> Improvements to the platform&rsquo;s user-friendly portal streamlined patient scheduling, exercise tracking, and access to educational resources.</li>
 		<li><b>Automated Communications:</b> A new system automatically sends text and email reminders, increasing patient engagement and adherence to treatment plans.</li>
 	</ul>
-	<h2>Future Roadmap</h2>
+	<h3>Future Roadmap</h3>
 	<p>Looking ahead, Cascadian Software also developed a roadmap for future improvements, including:</p>
 	<ul>
 		<li><b>API Layer:</b> Implementing an API layer for seamless communication between the platform&rsquo;s Progressive Web App (PWA) and their health management software.</li>
 		<li><b>HIPAA-Compliant Data Solutions:</b> Enhancing data security with a Google Firebase HIPAA-compliant database for safe and efficient patient data storage.</li>
 	</ul>
-	<h2>Results</h2>
+	<h3>Results</h3>
 	<p>The implementation of these solutions led to significant improvements:</p>
 	<ul>
 		<li><b>Enhanced Clinician Experience:</b> Automation reduced administrative tasks, allowing clinicians to focus more on patient care.</li>
@@ -46,13 +46,13 @@ export const OvercomingOperationalChallenges = `
 		<li><b>Reduced Administrative Load:</b> Automation dramatically reduced the time spent on administrative duties, freeing up more time for patient interactions.</li>
 		<li><b>Improved Data Access:</b> Clinicians now have quicker, more reliable access to patient data, supporting timely and informed decision-making.</li>
 	</ul>
-	<h2>Client Feedback</h2>
+	<h3>Client Feedback</h3>
 	<ul style="list-style-type: none;">
 		<li><i>&quot;Working with Cascadian Software has transformed our operations. Automation freed our team to focus on performance and patient engagement rather than manual tasks. It&rsquo;s changed how we work with health system partners and increased the value we deliver to patients.&quot;&nbsp;&ndash;</i> <b>Head of Product</b></li>
 		<br>
 		<li><i>&quot;Cascadian has been an incredible partner. They helped us automate metrics tracking, which saved time and improved team performance. This also enhanced our discussions with health system partners about the value we provide.&quot;&nbsp;&ndash;</i> <b>Head of Operations</b></li>
 	</ul>
-	<h2>Conclusion</h2>
+	<h3>Conclusion</h3>
 	<p>By partnering with Cascadian Software, the platform overcame key operational challenges, significantly enhancing clinician experience and efficiency through automation. As they continue to innovate in cardiac wellness, this collaboration will drive further improvements in healthcare delivery.</p>
 	<hr>
 
