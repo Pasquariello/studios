161626b171af79fbfa2eaf5dceacb0c89923702a
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-10-11 16:36:46 -0700

    fix solutions issue

--- utils/solutions/index.js
+++ utils/solutions/index.js
@@ -14,6 +14,6 @@ export const solutions = [
         title: `Overcoming Operational Challenges`,
         body: OvercomingOperationalChallenges,
         image: '/static/images/andrew-neel-coffee.jpg',
-        preview: 'We\'ve been hard at work delivering high quality software for our clients! We are currently writting new case studies to share with you as you\'re reading this.  If you\'d like to learn more send us an email so we can connect.  Thank you!'
+        preview: 'A leading virtual cardiac wellness platform aimed to improve both clinician experience and operational efficiency. After facing significant challenges in clinician and workflow management, the company partnered with Cascadian Software to introduce automation, streamline data access, and enhance technology infrastructure. These changes resulted in faster onboarding, reduced administrative burdens, and more informed decision-making, enabling clinicians to focus more on patient care.'
     },
 ];
