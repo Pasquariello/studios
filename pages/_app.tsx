35dc9ea525c09531fcc7f29e3658ddfcfb139994
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-12-12 11:02:38 -0800

    re add tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,28 +9,28 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
-      <Script type="text/javascript" id="linkedIn">
+      <Script id="linkedIn">
         {`
-_linkedin_partner_id = ${process.env.LINKEDIN_ID};
-window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-`}
-</Script>
-<Script type="text/javascript" id="linkedIn-tracking">
-{`
-(function(l) {
-if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-window.lintrk.q=[]}
-var s = document.getElementsByTagName("script")[0];
-var b = document.createElement("script");
-b.type = "text/javascript";b.async = true;
-b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-s.parentNode.insertBefore(b, s);})(window.lintrk);
-`}
-</Script>
-<noscript>
-<img height="1" width="1" style={{display:'none'}} alt="" src="https://px.ads.linkedin.com/collect/?pid=6683602&fmt=gif" />
-</noscript>
+          _linkedin_partner_id = ${process.env.LINKEDIN_ID};
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        `}
+      </Script>
+      <Script id="linkedIn-tracking">
+          {`
+          (function(l) {
+          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]}
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{display:'none'}} alt="" src={`https://px.ads.linkedin.com/collect/?pid=${process.env.LINKEDIN_ID}&fmt=gif`} />
+      </noscript>
     </>
   )
 }
