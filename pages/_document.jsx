f95e8cf8e84f5eca1e3a3b8ebcf59ede3de33ce0
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-12-16 16:26:18 -0800

    update issues with insights tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,31 +3,6 @@ import { Html, Head, Main, NextScript, Footer } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="shortcut icon" href="/static/images/favicon.png" />
-        <script script type="text/javascript">
-        {`
-          _linkedin_partner_id = 6683602;
-          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-        `}
-      </script>
-      <script script type="text/javascript">
-          {`
-          (function(l) {
-          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-          window.lintrk.q=[]}
-          var s = document.getElementsByTagName("script")[0];
-          var b = document.createElement("script");
-          b.type = "text/javascript";b.async = true;
-          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-          s.parentNode.insertBefore(b, s);})(window.lintrk);
-          `}
-      </script>
-      <noscript>
-        <img height="1" width="1" style={{display:'none'}} alt="" src="https://px.ads.linkedin.com/collect/?pid=6683602&fmt=gif" />
-      </noscript>
-      </Head>
       <body>
         <Main />
         <NextScript />
