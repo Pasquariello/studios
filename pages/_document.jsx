ad43d2ce0d34d9a1c7d8e39be7f62e4d4f3f57f4
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-12-16 16:27:51 -0800

    test

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript, Footer } from 'next/document'
 export default function Document() {
   return (
     <Html>
+      <Head>
+      </Head>
       <body>
         <Main />
         <NextScript />
